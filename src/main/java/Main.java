a2fbc6c3bb68a9c2bb88bfdbcd0a8ebf68abb87a
Mike Nöthiger <noethiger.mike@gmail.com>
2019-10-14 13:33:43 +0200

    cleanup and readme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,13 @@ public class Main {
 
         // Run calibration
         bayesSpamFilter.calibrate(0.0051, 0.96, 0.01);
-        //bayesSpamFilter.autoCalibrate(); //Before uncomment this line read the description of the method
 
-        Set<String> words = new HashSet<>();
+        // Before uncomment the following line, read the description of the method
+        // bayesSpamFilter.autoCalibrate();
+
         bayesSpamFilter.learn();
         Result result = bayesSpamFilter.runTest("test");
         System.out.println("Resultate");
         System.out.println(result);
-
     }
 }
